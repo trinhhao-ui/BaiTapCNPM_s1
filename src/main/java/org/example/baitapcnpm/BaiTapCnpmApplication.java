52c072373c28b4cbf40f62bde85b9d908540f84c
phuhao <trinhphuhao2108@gmail.com>
2026-09-19 08:31:30 +0700

    feat: initialize Spring Boot recruitment management project with Maven and H2 database configuration

---
+++ src/main/java/org/example/baitapcnpm/BaiTapCnpmApplication.java
@@ -0,0 +1,13 @@
+package org.example.baitapcnpm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaiTapCnpmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaiTapCnpmApplication.class, args);
+    }
+
+}
